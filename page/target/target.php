34bf849f4915cab147286df5b412302353f2d8ec
pace11 <ryanjoker87@gmail.com>
2019-02-27 10:42:51 +0700

    update baru

--- page/target/target.php
+++ page/target/target.php
@@ -30,8 +30,8 @@
                                     <tbody>
                                         <?php 
                                             $no = 1;
-                                            $query = mysqli_query($conn, "SELECT * FROM tbl_input2
-                                                                          JOIN tbl_input4 ON tbl_input2.nomer_pjn_spj=tbl_input4.nomer_pjn_spj");
+                                            $query = mysqli_query($conn, "SELECT * FROM tbl_target
+                                                                          JOIN tbl_input4 ON tbl_target.nomer_pjn_spj=tbl_input4.nomer_pjn_spj");
                                             while($data = mysqli_fetch_array($query)) { ?>
                                                 <tr>
                                                     <td><?= $no ?></td>
@@ -89,8 +89,8 @@
                                             <option style="display:none;">-- pilih salah satu --</option>
                                             <?php 
                                             $noPjn = mysqli_query($conn, "SELECT tbl_input4.nomer_pjn_spj as nomer FROM tbl_input4
-                                                                          LEFT JOIN tbl_input2 ON tbl_input4.nomer_pjn_spj=tbl_input2.nomer_pjn_spj
-                                                                          WHERE tbl_input2.nomer_pjn_spj IS NULL");
+                                                                          LEFT JOIN tbl_target ON tbl_input4.nomer_pjn_spj=tbl_target.nomer_pjn_spj
+                                                                          WHERE tbl_target.nomer_pjn_spj IS NULL");
                                             while($getnoPjn = mysqli_fetch_array($noPjn)){
                                                 echo "<option value='".$getnoPjn['nomer']."'>".$getnoPjn['nomer']."</option>";
                                             } 
@@ -106,11 +106,11 @@
                                     </div>
                                 </div>
                                 <div class="row">
-                                    <div class="col-md-8" id="tblsktm">
+                                    <div class="col-md-10" id="tblsktm">
                                     <?php
                                         $hitsktm  = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM tbl_sktm"));
                                     ?>
-                                    <input type="hidden" name="count" value="<?= $hitsktm ?>">
+                                    <input type="hidden" name="count" id="countsktm" value="<?= $hitsktm ?>">
                                         <table class="table table-bordered">
                                             <tbody>
                                             <tr>
@@ -124,6 +124,7 @@
                                             </tr>
                                             <?php
                                                 $no = 1;
+                                                $id = 0;
                                                 $query = mysqli_query($conn, "SELECT * FROM tbl_sktm");
                                                 while($data = mysqli_fetch_array($query)) {
                                             ?>
@@ -131,19 +132,28 @@
                                                     <td><?= $no ?></td>
                                                     <td><?= $data['id_sktm'] ?></td>
                                                     <td>
-                                                        <input type="text" class="form-control fieldsktm" name="hargasktm<?= $no ?>" id="hargasktm<?= $no ?>">
+                                                        <input type="number" class="form-control fieldsktm hargasktm" min="0" name="hargasktm<?= $no ?>" id="hargasktm<?= $id ?>" autocomplete="off">
                                                     </td>
                                                     <td>
-                                                        <input type="text" class="form-control fieldsktm" name="bobotsktm<?= $no ?>" id="bobotsktm<?= $no ?>" readonly>
+                                                        <input type="number" class="form-control fieldsktm" name="bobotsktm<?= $no ?>" id="bobotsktm<?= $id ?>" readonly>
                                                     </td>
                                                     <td>
-                                                        <input type="text" class="form-control fieldsktm" name="mulaikerjasktm<?= $no ?>">
+                                                        <input type="text" class="form-control fieldsktm" name="mulaikerjasktm<?= $no ?>" autocomplete="off">
                                                     </td>
                                                     <td>
-                                                        <input type="text" class="form-control fieldsktm" name="hari_kerjasktm<?= $no ?>">
+                                                        <input type="text" class="form-control fieldsktm" name="lamakerjasktm<?= $no ?>" autocomplete="off">
                                                     </td>
                                                 </tr>
-                                            <?php $no++; } ?>
+                                            <?php $no++; $id++; } ?>
+                                                <tr>
+                                                    <td colspan="2">Jumlah</td>
+                                                    <td>
+                                                        <input type="text" class="form-control jmlhargasktm" readonly>
+                                                    </td>
+                                                    <td>
+                                                        <input type="text" class="form-control" id="jmlbobotktm" readonly>
+                                                    </td>
+                                                </tr>
                                             </tbody>
                                         </table>
                                     </div>
@@ -152,7 +162,7 @@
                                     <?php
                                         $hitsipil  = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM tbl_sipil"));
                                     ?>
-                                    <input type="hidden" name="count" value="<?= $hitsipil ?>">
+                                    <input type="hidden" name="count" id="countsipil" value="<?= $hitsipil ?>">
                                         <table class="table table-bordered">
                                             <tbody>
                                             <tr>
@@ -165,6 +175,7 @@
                                             </tr>
                                             <?php
                                                 $no = 1;
+                                                $ids = 0;
                                                 $query = mysqli_query($conn, "SELECT * FROM tbl_sipil");
                                                 while($data = mysqli_fetch_array($query)) {
                                             ?>
@@ -172,19 +183,19 @@
                                                     <td><?= $no ?></td>
                                                     <td><?= $data['id_sipil'] ?></td>
                                                     <td>
-                                                        <input type="text" class="form-control fieldsipil" name="hargasipil<?= $no ?>">
+                                                        <input type="number" class="form-control fieldsipil hargasipil" id="hargasipil<?= $no ?>" name="hargasipil<?= $no ?>" autocomplete="off">
                                                     </td>
                                                     <td>
-                                                        <input type="text" class="form-control fieldsipil" name="bobotsipil<?= $no ?>" readonly>
+                                                        <input type="number" class="form-control fieldsipil" id="bobotsipil<?= $ids ?>" name="bobotsipil<?= $no ?>" readonly>
                                                     </td>
                                                     <td>
-                                                        <input type="text" class="form-control fieldsipil" name="mulaikerjasipil<?= $no ?>">
+                                                        <input type="text" class="form-control fieldsipil" name="mulaikerjasipil<?= $no ?>" autocomplete="off">
                                                     </td>
                                                     <td>
-                                                        <input type="text" class="form-control fieldsipil" name="hari_kerjasipil<?= $no ?>">
+                                                        <input type="text" class="form-control fieldsipil" name="lamakerjasipil<?= $no ?>" autocomplete="off">
                                                     </td>
                                                 </tr>
-                                            <?php $no++; } ?>
+                                            <?php $no++; $ids++; } ?>
                                             </tbody>
                                         </table>
                                     </div>
